2a0b1851c8bfc73a05924927fe79fbd70770246b
Voxen <peakcell@ostily.com>
2018-04-28 04:29:27 +0200

    Setup

---
+++ Assets/Obstacle.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour {
+    
+    //Name
+    public string name;
+    //Defines if the object is on the ground(0), in the air(1) or both(2)
+    public int type;
+    //Indicates if the obstacle is an attacking opponent(true) or not(false)
+    public bool attacks;
+    //Variable indicating damages done to Godzilla
+    public int attackDamage;
+    //Variable indicating damages done to Godzilla when colliding with the obstacle
+    public int collisionDamage;
+    //For obstacles health
+    public int healthPoints;
+    //Just in case
+    public int armorPoints;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Init(string nam, MeshFilter mesh, int typ, bool atk, int atkdmg, int coldmg, int hp, int armpts)
+    {
+        name = nam;
+        type = typ;
+        attacks = atk;
+        attackDamage = atkdmg;
+        collisionDamage = coldmg;
+        healthPoints = hp;
+        armorPoints = armpts;
+
+        GetComponent<MeshFilter>().sharedMesh = mesh.sharedMesh;
+    }
+}
---
+++ Assets/ObstacleGenerator.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleGenerator : MonoBehaviour
+{
+
+    [System.Serializable]
+    public class ObstacleStats : System.Object
+    {
+        //Name
+        public string name;
+        //Mesh
+        public MeshFilter mesh;
+        //Defines if the object is on the ground(0), in the air(1) or both(2)
+        public int type;
+        //Indicates if the obstacle is an attacking opponent(true) or not(false)
+        public bool attacks;
+        //Variable indicating damages done to Godzilla
+        public int attackDamage;
+        //Variable indicating damages done to Godzilla when colliding with the obstacle
+        public int collisionDamage;
+        //For obstacles health
+        public int healthPoints;
+        //Just in case
+        public int armorPoints;
+    }
+
+    public ObstacleStats[] obstacles;
+
+    public GameObject Roads;
+
+    public Vector3 minPos;
+    public Vector3 maxPos;
+
+    public float Timer;
+    public float TimerMin;
+    public float TimerMax;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Timer -= Time.deltaTime;
+        if (Timer <= 0)
+        {
+            Timer = Random.Range(TimerMin, TimerMax);
+            PlaceObstacle(0);
+        }
+    }
+
+    private void PlaceObstacle(int id)
+    {
+        int indexRoad = Random.Range(0, Roads.transform.childCount);
+        ObstacleStats obs = obstacles[id];
+        GameObject obj = new GameObject();
+        //obj = obs.mesh;
+        obj.AddComponent<MeshFilter>();
+        obj.AddComponent<MeshRenderer>();
+        obj.AddComponent<Obstacle>();
+        obj.GetComponent<Obstacle>().Init(obs.name, obs.mesh, obs.type, obs.attacks, obs.attackDamage, obs.collisionDamage, obs.healthPoints, obs.armorPoints);
+        obj.name = obs.name;
+        obj.transform.position = Roads.transform.GetChild(indexRoad).transform.position;
+    }
+}
